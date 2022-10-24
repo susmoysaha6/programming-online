484096463f1332955ef65ebf0415a25bb551b3ce
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-24 23:17:24 +0600

    favicon , title, navbar logo and name changed

--- src/components/Headers/Headers.js
+++ src/components/Headers/Headers.js
@@ -1,6 +1,7 @@
 import { Button, DarkThemeToggle, Navbar } from 'flowbite-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import po from '../../assets/po.png'
 
 const Headers = () => {
     return (
@@ -10,12 +11,12 @@ const Headers = () => {
         >
             <Navbar.Brand href="https://flowbite.com/">
                 <img
-                    src="https://flowbite.com/docs/images/logo.svg"
+                    src={po}
                     className="mr-3 h-6 sm:h-9"
-                    alt="Flowbite Logo"
+                    alt=""
                 />
                 <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                    Flowbite
+                    Programming Online
                 </span>
             </Navbar.Brand>
             <div className="flex md:order-2">
