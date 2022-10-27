75ae514f7048253122161c8b8bc6934eff675b79
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-27 16:59:21 +0600

    home page added

--- src/components/FooterContainer/FooterContainer.js
+++ src/components/FooterContainer/FooterContainer.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { FaFacebook, FaFacebookMessenger, FaGithub, FaGithubAlt, FaGoogle, FaInstagram, FaInstagramSquare, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+import { FaFacebook, FaFacebookMessenger, FaGithub, FaGoogle, FaInstagram, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+
+import po from '../../assets/po.png'
 
 const FooterContainer = () => {
     return (
         <footer className='mt-10  text-slate-800 shadow-2xl py-5 dark:bg-sky-400 '>
+            <img width={50} className='mx-auto my-5' src={po} alt="" />
             <h3 className='text-center text-2xl font-bold'>Programming Online</h3>
             <p className='text-center my-2 text-xl font-semibold'>Contact Us:</p>
             <div className='flex justify-evenly md:w-1/2 lg:w-1/3 mx-auto text-xl
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import prog from '../../assets/prog.png'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            This is Home
+            <section className="w-full lg:grid grid-cols-2 dark:bg-gray-800 dark:text-gray-100 my-12">
+                <div className=''>
+                    <img className='w-full' src={prog} alt="" />
+                </div>
+                <div className='p-12  lg:w-3/4'>
+                    <h3 className='text-3xl font-bold'>Programming Online</h3>
+                    <p className='py-10 md:text-xl font-semibld'>It is a online based programming or technology learning platform on verious technology/ programming languages by world famous instractor. We have many facilitics, like: student support, job placement. We provide certificates after completeing the course successfully.</p>
+                    <div className='mt-10'>
+                        <Link to='/courses' className='p-4 rounded bg-sky-400 text-slate-900'>Select from our courses</Link>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
